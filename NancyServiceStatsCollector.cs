6e4b978253e0061bc84c53f1f9e3442b937f8c99
jsolana_cr <jsolana@rfranco.com>
2019-11-07 15:37:19 +0100

    Custom metrics renamed.

--- NancyServiceStatsCollector.cs
+++ NancyServiceStatsCollector.cs
@@ -10,11 +10,11 @@ namespace Com.RFranco.Iris.NancyMetrics.Stats
     {
         private const string requestDateTimeNancyContextItem = "RequestDateTime";
 
-        private static readonly Counter RequestsProcessed = Metrics.CreateCounter("total_successfull_requests", "Number of successfull processed requests.", "method");
-        private static readonly Counter ErrorRequestsProcessed = Metrics.CreateCounter("total_error_responses", "Number of unsuccessfull processed requests.", "method", "code_error");
-        private static readonly Gauge OngoingRequests = Metrics.CreateGauge("requests_in_progress", "Number of ongoing requests.", "method");
-        private static readonly Summary RequestsDurationSummaryInSeconds = Metrics.CreateSummary("requests_duration_summary_seconds", "A Summary of request duration (in seconds) over last 10 minutes.", "method");
-        private static readonly Histogram RequestResponseHistogram = Metrics.CreateHistogram("requests_duration_histogram_seconds", "Histogram of request duration in seconds.", "method");
+        private static readonly Counter RequestsProcessed = Metrics.CreateCounter("http_requests_total", "Number of successfull processed requests.", "method");
+        private static readonly Counter ErrorRequestsProcessed = Metrics.CreateCounter("http_error_total", "Number of unsuccessfull processed requests.", "method", "code_error");
+        private static readonly Gauge OngoingRequests = Metrics.CreateGauge("http_requests_in_progress", "Number of ongoing requests.", "method");
+        private static readonly Summary RequestsDurationSummaryInSeconds = Metrics.CreateSummary("http_requests_duration_summary_seconds", "A Summary of request duration (in seconds) over last 10 minutes.", "method");
+        private static readonly Histogram RequestResponseHistogram = Metrics.CreateHistogram("http_requests_duration_histogram_seconds", "Histogram of request duration in seconds.", "method");
 
         public static void UpdateMetrics(IPipelines pipelines, NancyContext context)
         {
